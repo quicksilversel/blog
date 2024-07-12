077d6994febcb8ec23485ffd53032f7ea9f97b2d
quicksilversel <quicksilversel@gmail.com>
2024-07-13 08:55:38 +0900

    add: mdx

---
+++ src/components/mdx/H1/H1.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styled from '@emotion/styled'
+
+export default function H1({ children }: { children?: React.ReactNode }) {
+  return <StyledH1 className="mdx-h1">{children}</StyledH1>
+}
+
+const StyledH1 = styled.h1`
+  font-size: 2rem;
+  font-weight: 700;
+  margin: 1rem 0;
+`
--- src/pages/articles/[...slug].tsx
+++ src/pages/articles/[...slug].tsx
@@ -1,13 +1,49 @@
-type Props = {
-  slug: string;
-};
+import fs from 'fs'
 
+import React from 'react'
 
-export default function Article() {
+import { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
+import Head from 'next/head'
+import { MDXRemote } from 'next-mdx-remote'
+import { serialize } from 'next-mdx-remote/serialize'
 
+import H1 from '@/components/mdx/H1/H1'
+
+export default function Article({
+  source,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
-      <p>{1}</p>
+      <Head>
+        <title>{source.frontmatter.title as string}</title>
+      </Head>
+      <MDXRemote
+        {...source}
+        components={{
+          h1: H1,
+        }}
+      />
     </>
   )
+}
+export async function getStaticPaths() {
+  return { paths: [], fallback: 'blocking' }
+}
+
+export async function getStaticProps(
+  ctx: GetStaticPropsContext<{
+    slug: string
+  }>,
+) {
+  const { slug } = ctx.params!
+
+  const postFile = fs.readFileSync(`articles/articles/${slug}.mdx`)
+
+  const mdxSource = await serialize(postFile, { parseFrontmatter: true })
+  return {
+    props: {
+      source: mdxSource,
+    },
+    revalidate: 60,
+  }
 }
