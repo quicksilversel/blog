79aa0537499e8f1c6805a3ceba70a05622c68082
quicksilversel <quicksilversel@gmail.com>
2024-07-12 17:52:39 +0900

    initial commit

---
+++ src/pages/articles/[...slug].tsx
@@ -0,0 +1,13 @@
+type Props = {
+  slug: string;
+};
+
+
+export default function Article() {
+
+  return (
+    <>
+      <p>{1}</p>
+    </>
+  )
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import styled from '@emotion/styled'
+
+export default function Index() {
+  return <DefaultLayout>hello</DefaultLayout>
+}
+
+const DefaultLayout = styled.main`
+  max-width: 1000px;
+  margin: 0 auto;
+`
